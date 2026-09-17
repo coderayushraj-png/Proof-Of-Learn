7d2ba9f43c6411c751a87c2db196125e1d6cb28b
Ayush <coderayushraj@gmail.com>
2026-09-17 13:48:10 +0530

    chore(deps): refactor package dependencies and cleanup
    - Move development dependencies to standard dependencies
    - Update versions for Vite, Tailwind, and TypeScript
    - Remove obsolete cleanup and maintenance scripts

--- check_deps.cjs
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
-const deps = Object.keys(pkg.dependencies || {});
-const devDeps = Object.keys(pkg.devDependencies || {});
-console.log('Dependencies to check:', deps);
